e8147afa7d369fbe4857c31520c6c2cbd8d29086
Alejandro Montilla <luis.alejandro.499be@gmail.com>
2026-04-19 12:05:48 +0200

    Fix build bug

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,9 +1,9 @@
 'use client';
 
+import i18n from '../src/i18n/i18n.js';
+import { I18nextProvider } from 'react-i18next';
 import * as React from 'react';
 import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from 'next-themes';
-import { I18nextProvider } from 'react-i18next';
-import i18n from '../src/i18n/i18n.js';
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
 	return (
